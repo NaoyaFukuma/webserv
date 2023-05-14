8f2c6beac69148f6e2030808eeaad6b87381ac3b
chanma <yunosuke.homma@icloud.com>
2023-05-15 03:57:36 +0900

    ファイル分けた <理由> #define BOOST_TEST_MODULE TestCustom1 // ... #define BOOST_TEST_MODULE TestCustom2 これは誤りです。Boost.Testのドキュメンテーションによれば、一つのテストバイナリあたり一つのテストモジュールしか持つことができません。BOOST_TEST_MODULEマクロはテストモジュールの名前を定義しますが、これは一つの実行可能ファイル内で一度だけ定義すべきです。

---
+++ unit-test/config/TestCase1.cpp
@@ -0,0 +1,30 @@
+//
+// Created by chanma on 2023/05/15.
+//
+#define BOOST_TEST_DYN_LINK
+
+#include <arpa/inet.h>
+#include "Config.hpp"
+#include "ConfigParser.hpp"
+
+#define CASE1 "../../../unit-test/config/test_config_files/custom/case1.conf"
+
+/* ================================== CASE1 ================================== */
+
+#define BOOST_TEST_MODULE TestCase1
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(Case1) {
+        boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_messages);
+
+        Config conf;
+        try {
+          conf.ParseConfig(CASE1);
+          BOOST_FAIL("Expected ParserException to be thrown but it was not.");
+        } catch (const ConfigParser::ParserException& e) {
+          BOOST_TEST_MESSAGE("Caught expected exception: " << e.what());
+          BOOST_CHECK(true);
+        } catch (...) {
+          BOOST_FAIL("Caught unexpected exception.");
+        }
+}
