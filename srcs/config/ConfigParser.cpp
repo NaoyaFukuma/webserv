4e85ac276ca4e3b28ce7f137315c52e893228c93
chanma <yunosuke.homma@icloud.com>
2023-05-17 11:47:22 +0900

    c_strを追加

--- srcs/config/ConfigParser.cpp
+++ srcs/config/ConfigParser.cpp
@@ -35,7 +35,7 @@ std::string ConfigParser::LoadFile(const char *filepath) {
   // filepathの拡張子を確認
   std::string ext = GetFileExt(filepath);
   if (ext != "conf") {
-    throw ParserException("Config Error: invalid file extension: %s", ext);
+    throw ParserException("Config Error: invalid file extension: %s", ext.c_str());
   }
 
   // ファイルを読み込み、stringに変換
