175d152d70bdec45779dc80af9b63a16782251f3
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-05-22 12:10:22 +0900

    .tppのインクルードをヘッダー末尾に移動し、テンプレート関数のプロトタイプをヘッダーに記載

--- srcs/utils/utils.hpp
+++ srcs/utils/utils.hpp
@@ -1,7 +1,6 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include "utils.tpp"
 #include <stdint.h>
 #include <string>
 #include <sys/types.h>
@@ -13,5 +12,9 @@ bool ws_inet_addr(uint32_t &dst, std::string ip);
 void *ws_memset(void *s, int c, size_t n);
 void *ws_memcpy(void *dest, const void *src, size_t n);
 bool ws_isspace(const char c);
+template <typename T> bool ws_strtoi(T *dest, const std::string src);
+template <typename T> T mul_assert_overflow(T lhs, T rhs);
+
+#include "utils.tpp"
 
 #endif
