71bf89f4dbd69f962fdbfa3bf79a5381995b0f47
tfuji <tomofuji.dev@gmail.com>
2023-05-23 10:52:51 +0900

    multiple location test caseを追加

--- unit-test/request/TestResolvePath.cpp
+++ unit-test/request/TestResolvePath.cpp
@@ -4,9 +4,9 @@
 #include "Request.hpp"
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(TestResolveResourcePath) {
+BOOST_AUTO_TEST_CASE(SimpleConfig) {
   Config config;
-  config.ParseConfig("../../config/test_config_files/valid/simple.conf");
+  config.ParseConfig("../../config/test_config_files/valid/Simple.conf");
   std::cout << config << std::endl;
   Request request;
   RequestMessage message = {.request_line = {.method = "GET",
@@ -16,7 +16,38 @@ BOOST_AUTO_TEST_CASE(TestResolveResourcePath) {
                             .body = ""};
   request.SetMessage(message);
   request.ResolvePath(config);
-  std::cout << request.GetContext().resource_path.server_path << std::endl;
   BOOST_CHECK_EQUAL(request.GetContext().resource_path.server_path,
                     "/var/www/html/test/index.html");
+}
+
+BOOST_AUTO_TEST_CASE(SimpleConfigWithoutSlash) {
+  Config config;
+  config.ParseConfig("../../config/test_config_files/valid/Simple.conf");
+  std::cout << config << std::endl;
+  Request request;
+  RequestMessage message = {.request_line = {.method = "GET",
+                                             .uri = "test/index.html",
+                                             .version = Http::HTTP11},
+                            .header = {},
+                            .body = ""};
+  request.SetMessage(message);
+  request.ResolvePath(config);
+  BOOST_CHECK_EQUAL(request.GetContext().resource_path.server_path,
+                    "/var/www/html/test/index.html");
+}
+
+BOOST_AUTO_TEST_CASE(MultipleLocation) {
+  Config config;
+  config.ParseConfig("../../config/test_config_files/valid/MultipleRoute.conf");
+  std::cout << config << std::endl;
+  Request request;
+  RequestMessage message = {.request_line = {.method = "GET",
+                                             .uri = "/b/a/index.html",
+                                             .version = Http::HTTP11},
+                            .header = {},
+                            .body = ""};
+  request.SetMessage(message);
+  request.ResolvePath(config);
+  BOOST_CHECK_EQUAL(request.GetContext().resource_path.server_path,
+                    "/var/www/html/b/a/index.html");
 }
