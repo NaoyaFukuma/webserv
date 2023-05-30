175a4f75dcdca0af487a29387732de16b73af13c
tfuji <tomofuji.dev@gmail.com>
2023-05-30 12:29:17 +0900

    merge

--- srcs/http/Response.cpp
+++ srcs/http/Response.cpp
@@ -276,8 +276,246 @@ bool Response::StaticFileBody(const std::string &path) {
   SetBody(body);
 
   // content-typeを設定
+<<<<<<< HEAD
   SetHeader("Content-Type",
             std::vector<std::string>(1, ws_get_mime_type(path.c_str())));
+||||||| parent of 15645d4 (if~~のheaderチェックを追加)
+  // SetHeader("Content-Type",
+  //           std::vector<std::string>(1, ws_get_mime_type(path.c_str())));
+
+  // content-lengthを設定
+  std::stringstream ss;
+  ss << body_size;
+  SetHeader("Content-Length", ss.str());
+
+  // // Check If headers.
+  // iIfMod = IfModSince(hInfo, sBuf.st_mtime);
+  // iIfUnmod = IfUnmodSince(hInfo, sBuf.st_mtime);
+  // iIfMatch = IfMatch(hInfo, sBuf.st_mtime);
+  // iIfNone = IfNone(hInfo, sBuf.st_mtime);
+  // iIfRange = IfRange(hInfo, sBuf.st_mtime);
+  // iRangeErr = hInfo->FindRanges(sBuf.st_size);
+
+  // // Check to make sure any If headers are FALSE.
+  // // Either not-modified or no etags matched.
+  // if ((iIfMod == FALSE) || (iIfNone == FALSE)) {
+  //   sClient->Send("HTTP/1.1 304 Not Modified\r\n");
+  //   iRsp = 304;
+  // }
+  // // No matching etags or it's been modified.
+  // else if ((iIfMatch == FALSE) || (iIfUnmod == FALSE)) {
+  //   sClient->Send("HTTP/1.1 412 Precondition Failed\r\n");
+  //   iRsp = 412;
+  // }
+  // // Resource matched so send just the bytes requested.
+  // else if ((iIfRange == TRUE) && (iRangeErr == 0)) {
+  //   sClient->Send("HTTP/1.1 206 Partial Content\r\n");
+  //   iRsp = 206;
+  // }
+  // // Resource didn't match, so send the entire entity.
+  // else {
+  //   sClient->Send("HTTP/1.1 200 OK\r\n");
+  //   iRsp = 200;
+  // }
+=======
+  // SetHeader("Content-Type",
+  //           std::vector<std::string>(1, ws_get_mime_type(path.c_str())));
+
+  // content-lengthを設定
+  std::stringstream ss;
+  ss << body_size;
+  SetHeader("Content-Length", ss.str());
+}
+
+bool Response::IsValidFile(Request &request, const std::string &path) {
+  // Check If headers.
+  bool iIfMod = IfModSince(request, path);
+  bool iIfUnmod = IfUnmodSince(request, path);
+  bool iIfMatch = IfMatch(request, path);
+  bool iIfNone = IfNone(request, path);
+  bool iIfRange = IfRange(request, path);
+  std::vector<std::pair<size_t, size_t>> ranges;
+  bool iFindRange = FindRanges(request, path, ranges);
+
+  // Check to make sure any If headers are FALSE.
+  // Either not-modified or no etags matched.
+  if ((iIfMod == false) || (iIfNone == false)) {
+    SetResponseStatus(304);
+    return false;
+  }
+  // No matching etags or it's been modified.
+  else if ((iIfMatch == false) || (iIfUnmod == false)) {
+    SetResponseStatus(412);
+    return false;
+  }
+  // Resource matched so send just the bytes requested.
+  else if ((iIfRange == true) && (iFindRange == true)) {
+    SetResponseStatus(206);
+    // Todo: set partial
+    // body(バイトレンジを指定している場合、bodyに指定された部分のみを設定する)
+    return false;
+  }
+  // Resource didn't match, so send the entire entity.
+  else {
+    return true;
+  }
+}
+
+bool Response::IfModSince(Request &request, const std::string &path) {
+  // If-Modified-Since
+  if (request.HasHeader("If-Modified-Since") == false ||
+      request.GetHeader("If-Modified-Since").empty()) {
+    return true;
+  }
+  std::string if_mod_since_str = request.GetHeader("If-Modified-Since")[0];
+  std::time_t if_mod_since = str2time(if_mod_since_str);
+  if (if_mod_since == -1) {
+    return true;
+  }
+  std::time_t last_mod = GetLastModified(path);
+  return last_mod > if_mod_since;
+}
+
+bool Response::IfUnmodSince(Request &request, const std::string &path) {
+  if (request.HasHeader("If-Unmodified-Since") == false ||
+      request.GetHeader("If-Unmodified-Since").empty()) {
+    return true;
+  }
+  std::string if_unmod_since_str = request.GetHeader("If-Unmodified-Since")[0];
+  std::time_t if_unmod_since = str2time(if_unmod_since_str);
+  if (if_unmod_since == -1) {
+    return true;
+  }
+  std::time_t last_mod = GetLastModified(path);
+  return last_mod < if_unmod_since;
+}
+
+bool Response::IfMatch(Request &request, const std::string &path) {
+  if (request.HasHeader("If-Match") == false ||
+      request.GetHeader("If-Match").empty()) {
+    return true;
+  }
+  std::vector<std::string> if_match_vec = request.GetHeader("If-Match");
+  std::string etag = GetEtag(path);
+  for (std::string::iterator if_match = if_match_vec.begin();
+       if_match != if_match_vec.end(); if_match++) {
+    if (*if_match == "*") {
+      return true;
+    }
+    if (*if_match == etag) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Response::IfNone(Request &request, const std::string &path) {
+  if (request.HasHeader("If-None-Match") == false ||
+      request.GetHeader("If-None-Match").empty()) {
+    return true;
+  }
+  std::vector<std::string> if_none_vec = request.GetHeader("If-None-Match");
+  std::string etag = GetEtag(path);
+  for (std::string::iterator if_none = if_none_vec.begin();
+       if_none != if_none_vec.end(); if_none++) {
+    if (*if_none == "*") {
+      return false;
+    }
+    if (*if_none == etag) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool Response::IfRange(Request &request, const std::string &path) {
+  if (request.HasHeader("If-Range") == false ||
+      request.GetHeader("If-Range").empty()) {
+    return false;
+  }
+  std::string if_range_str = request.GetHeader("If-Range")[0];
+  // If-Range could be a date or an ETag.
+  // Try to parse as date first.
+  std::time_t if_range_date = str2time(if_range_str);
+  if (if_range_date != -1) {
+    std::time_t last_mod = GetLastModified(path);
+    return last_mod == if_range_date;
+  } else {
+    // Try to parse as ETag
+    std::string etag = GetEtag(path);
+    return etag == if_range_str;
+  }
+}
+
+bool Response::FindRanges(std::vector<std::pair<size_t, size_t>> &ranges,
+                          Request &request, const std::string &path) {
+  if (request.HasHeader("Range") == false ||
+      request.GetHeader("Range").empty()) {
+    return false;
+  }
+  std::string range_str = request.GetHeader("Range")[0];
+  // if range_str start with "bytes="
+  if (range_str.find("bytes=") != 0) {
+    return false;
+  }
+  range_str = range_str.substr(6);
+  std::vector<std::string> range_vec;
+  if (ws_split(range_vec, range_str, ',') <= 0) {
+    return false;
+  }
+  struct stat st;
+  if (stat(path.c_str(), &st) != 0) {
+    return false;
+  }
+  for (std::vector<std::string>::iterator range = range_vec.begin();
+       range != range_vec.end(); range++) {
+    std::vector<std::string> range_pair;
+    if (ws_split(range_pair, *range, '-') != 2) {
+      return false;
+    }
+    std::string range_start_str = range_pair[0];
+    std::string range_end_str = range_pair[1];
+    if (range_start_str.empty() && range_end_str.empty()) {
+      return false;
+    }
+    if (range_start_str.empty()) {
+      range_start_str = "0";
+    }
+    if (range_end_str.empty()) {
+      range_end_str = std::to_string(st.st_size - 1);
+    }
+    std::size_t range_start;
+    if (ws_strtoi<size_t>(range_start, range_start_str) == false) {
+      return false;
+    }
+    std::size_t range_end;
+    if (ws_strtoi<size_t>(range_end, range_end_str) == false) {
+      return false;
+    }
+    if (range_start > range_end) {
+      return false;
+    }
+    if (range_end > st.st_size - 1) {
+      return false;
+    }
+    ranges.push_back(std::make_pair(range_start, range_end));
+  }
+  return true;
+}
+
+std::time_t Response::GetLastModified(const std::string &path) {
+  struct stat st;
+  if (stat(path.c_str(), &st) != 0) {
+    return 0;
+  }
+  return st.st_mtime;
+}
+
+std::string Response::GetEtag(const std::string &path) {
+  std::stringstream ss;
+  ss << std::hex << file_stat.st_mtime << "-" << file_stat.st_size;
+  return ss.str();
+>>>>>>> 15645d4 (if~~のheaderチェックを追加)
 }
 
 bool Response::IsGetableFile(Request &request, const std::string &path) {
