a239b727b0db0ce8cf47e77275525817e41a0a13
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-06-05 13:29:13 +0900

    CGIスクリプトの内容を変更 標準エラーからの出力を追加し、ターミナル上でCGIの実行がわかるように変更（デバックのため）

--- www/webserv_com/cgi_bin/hello_world.c
+++ www/webserv_com/cgi_bin/hello_world.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
 
 int main(void) {
   printf("Status: 200 OK\r\n");
   printf("Content-type: text/html\r\n");
   printf("Content-Length: 48\r\n\r\n");
   printf("<html><body><h1>Hello, World!</h1></body></html>\r\n");
+  write(STDERR_FILENO, "Hello, World!\n", 14);
+  write(STDERR_FILENO, "CGI script finished\n", 20);
+
   return 0;
 }
