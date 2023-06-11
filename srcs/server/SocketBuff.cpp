7113d184d3d397afcba0f6e1568370ed112c9930
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-06-11 23:19:17 +0900

    vector<char>を使ってバッファリングするように修正

--- srcs/server/SocketBuff.cpp
+++ srcs/server/SocketBuff.cpp
@@ -1,128 +1,110 @@
 #include "SocketBuff.hpp"
-
-#include "define.hpp"
-#include <fstream>
-#include <ios>
 #include <iostream>
 #include <sys/socket.h>
 
-SocketBuff::SocketBuff() {}  // デフォルトコンストラクタ
-SocketBuff::~SocketBuff() {} // デストラクタ
-
 // FAILURE -1: socketを閉じる, SUCCESS 0: socketを閉じない
 int SocketBuff::ReadSocket(int fd) {
-  char buf[kBuffSize];
-  ssize_t len;
+    char buf[kBuffSize];
+    ssize_t len;
+    ssize_t total_len = 0;
 
-  while (true) {
-    len = recv(fd, buf, sizeof(buf), MSG_DONTWAIT);
+    while (true) {
+        len = recv(fd, buf, sizeof(buf), MSG_DONTWAIT);
     // recv()がエラーを返すか、リモートがコネクションを閉じた場合にループを抜ける
-    if (len <= 0) {
-      break;
+        if (len <= 0) {
+            break;
+        }
+        buffer_.insert(buffer_.end(), buf, buf + len);
+        total_len += len;
+        if (len < static_cast<ssize_t>(sizeof(buf))) {
+            break;
+        }
     }
-    ss_ << std::string(buf, len);
-    if (len < static_cast<ssize_t>(sizeof(buf))) {
-      break;
-    }
-  }
   // len == 0の時、リモートがコネクションを閉じたことを意味する
   return len != 0 ? SUCCESS : FAILURE;
 }
 
 bool SocketBuff::GetLine(std::string &line) {
-  std::getline(ss_, line);
-
-  std::ios_base::iostate state = ss_.rdstate();
-  if (state & std::ios_base::eofbit) {
-    return false;
-  } else if (state & std::ios_base::badbit) {
-    throw std::runtime_error("SocketBuff::GetLine() failed");
-  }
-  return true;
+    std::vector<char>::iterator it = std::find(buffer_.begin() + read_position_, buffer_.end(), '\n');
+    if (it == buffer_.end()) {
+        return false;
+    }
+    line.assign(buffer_.begin() + read_position_, it);
+    read_position_ = std::distance(buffer_.begin(), it) + 1;
+    return true;
 }
 
-void SocketBuff::ResetSeekg() { ss_.seekg(0); }
+void SocketBuff::ResetSeekg() {
+    read_position_ = 0;
+}
 
-void SocketBuff::ResetSeekp() { ss_.seekp(0); }
+void SocketBuff::ResetSeekp() {
+    // 不要になった
+}
 
 ssize_t SocketBuff::FindString(const std::string &target) {
-  ssize_t original_pos = static_cast<ssize_t>(ss_.tellg());
-  ssize_t target_pos = -1;
-  std::size_t target_index = 0;
-  char ch;
-
-  while (ss_.get(ch)) {
-    if (ch == target[target_index]) {
-      ++target_index;
-      if (target_index == target.size()) {
-        target_pos = static_cast<ssize_t>(ss_.tellg()) -
-                     static_cast<ssize_t>(target.size());
-        break;
-      }
-    } else {
-      target_index = 0;
+    std::vector<char>::iterator it = std::search(buffer_.begin() + read_position_, buffer_.end(), target.begin(), target.end());
+    if (it == buffer_.end()) {
+        return -1;
     }
-  }
-
-  // Reset the stream position to the original position
-  ss_.clear();
-  ss_.seekg(original_pos, std::ios_base::beg);
-
-  return target_pos;
+    return std::distance(buffer_.begin(), it);
 }
 
-std::string SocketBuff::GetAndErase(const std::size_t pos) {
-  std::string str = ss_.str().substr(0, pos);
-  ss_.str(ss_.str().erase(0, pos));
-  ss_.seekg(0, std::ios::beg);
-  ss_.seekp(0, std::ios::beg);
-  return str;
+std::string SocketBuff::GetAndErase(std::size_t pos) {
+    std::string str(buffer_.begin(), buffer_.begin() + pos);
+    buffer_.erase(buffer_.begin(), buffer_.begin() + pos);
+    read_position_ -= pos;
+    return str;
 }
 
 bool SocketBuff::GetUntilCRLF(std::string &line) {
-  ssize_t pos = FindString("\r\n");
-  if (pos == -1) {
-    return false;
-  }
-  line = GetAndErase(pos);
-  Erase(2); // "\r\n"を削除
-  return true;
-}
-
-void SocketBuff::Erase() {
-  ss_.str(ss_.str().erase(0, ss_.tellg()));
-  ss_.seekg(0, std::ios::beg);
-  ss_.seekp(0, std::ios::beg);
-}
-
-void SocketBuff::Erase(std::size_t n) {
-  ss_.str(ss_.str().erase(0, n));
-  ss_.seekg(0, std::ios::beg);
-  ss_.seekp(0, std::ios::beg);
+    const char CRLF[] = {'\r', '\n'};
+    std::vector<char>::iterator it = std::search(buffer_.begin() + read_position_, buffer_.end(), CRLF, CRLF + 2);
+    if (it == buffer_.end()) {
+        return false;
+    }
+    line.assign(buffer_.begin() + read_position_, it);
+    read_position_ = std::distance(buffer_.begin(), it) + 2;
+    return true;
 }
 
 std::size_t SocketBuff::GetReadSize() {
-  return ss_.tellg() == -1 ? 0 : static_cast<std::size_t>(ss_.tellg());
+    return buffer_.size() - read_position_;
 }
 
-void SocketBuff::AddString(const std::string &str) { ss_ << str; }
+void SocketBuff::Erase() {
+    buffer_.clear();
+    read_position_ = 0;
+}
 
-std::string SocketBuff::GetString() { return ss_.str(); }
+void SocketBuff::Erase(std::size_t n) {
+    buffer_.erase(buffer_.begin(), buffer_.begin() + n);
+    read_position_ -= n;
+}
+
+void SocketBuff::AddString(const std::string &str) {
+    buffer_.insert(buffer_.end(), str.begin(), str.end());
+}
+
+std::string SocketBuff::GetString() {
+    return std::string(buffer_.begin() + read_position_, buffer_.end());
+}
+
+int SocketBuff::SendSocket(int fd) {
+    ssize_t len = static_cast<ssize_t>(buffer_.size());
+    ssize_t send_len = send(fd, &buffer_[0], len, MSG_DONTWAIT);
+    if (send_len < 0) {
+        return -1;
+    }
+    Erase(send_len);
+    return send_len == len;
+}
+
+std::size_t SocketBuff::GetBuffSize() {
+    return buffer_.size();
+}
 
 void SocketBuff::ClearBuff() {
-  ss_.str("");
-  ss_.seekg(0, std::ios::beg);
-  ss_.seekp(0, std::ios::beg);
+    buffer_.clear();
+    read_position_ = 0;
 }
-
-int SocketBuff::SendSocket(const int fd) {
-  ssize_t len = static_cast<ssize_t>(ss_.str().size());
-  ssize_t send_len = send(fd, ss_.str().c_str(), len, MSG_DONTWAIT);
-  if (send_len < 0) {
-    return -1;
-  }
-  Erase(send_len);
-  return send_len == len;
-}
-
-std::size_t SocketBuff::GetBuffSize() { return ss_.str().size(); }
--- srcs/server/SocketBuff.hpp
+++ srcs/server/SocketBuff.hpp
@@ -1,62 +1,60 @@
 #ifndef SOCKETBUFF_HPP_
 #define SOCKETBUFF_HPP_
 
-#include <sstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <sys/types.h>
 
 class SocketBuff {
 private:
-  std::stringstream ss_;
-  const static std::size_t kBuffSize = 1024;
+    std::vector<char> buffer_;
+    std::size_t read_position_;
+    static const std::size_t kBuffSize = 1024;
 
 public:
-  SocketBuff(); // デフォルトコンストラクタ
-  ~SocketBuff();
+    SocketBuff() : read_position_(0) {}
+    ~SocketBuff() {}
 
   // EPOLLINを検知したソケットFDを受け取り、全てバッファに格納する
   // SUCCESS(0): ソケット内で読み込み可能な文字をすべてバッファに格納した
   // FAILURE(-1):
   // EPOLLINが発火して一回目のrecv()で0が返った。すなわち、finパケットの受信によりEPOLLINが発火したことを示す。
-  int ReadSocket(int fd);
+    int ReadSocket(int fd);
 
   // バッファから1行読み込み、引数に受け取った文字列に格納する
   // 戻り値がtrueだと、lineに読み込んだ文字列が入る
   // 戻り値falseだとeofに達している
   // failbitは考慮しないものとする（文字列からstring型のため）
   // badbitの場合は、例外を投げmainでcatchして終了する
-  bool GetLine(std::string &line);
-
-  // バッファのseekgを先頭に戻す
-  void ResetSeekg();
-  // バッファのseekpを先頭に戻す
-  void ResetSeekp();
+    bool GetLine(std::string &line);
+    void ResetSeekg();
+    void ResetSeekp();
 
   // バッファから任意の文字列があるか検索する 引数は検索する文字列
   // 戻り値は見つかった場合はバッファの中のその文字列の先頭のポジション、見つからない場合は-1
   // 利用のケースは、HTTPリクエストの終了”\r\n\r\n”を検索する
-  ssize_t FindString(const std::string &target);
+    ssize_t FindString(const std::string &target);
 
   // 引数のseekgの位置の一文字前までの文字列を返し、バッファから削除する
-  // 戻り値(リクエストヘッダー)をパーサーに渡し、パーサーはstringstreamを利用してパースする
   std::string GetAndErase(const std::size_t pos);
 
-  bool GetUntilCRLF(std::string &line);
+    bool GetUntilCRLF(std::string &line);
 
   // 読み込んだ文字数を返す 現在のseekgの位置を利用している
-  std::size_t GetReadSize();
+    std::size_t GetReadSize();
 
   // 先頭から現在のseek位置までの文字数削除する
-  void Erase();
+    void Erase();
 
   // 先頭から任意の文字数削除する 引数は削除する文字数
   void Erase(const std::size_t n);
 
   // バッファに文字列を追加する
-  void AddString(const std::string &str);
+    void AddString(const std::string &str);
 
   // バッファの文字列を返す
-  std::string GetString();
+    std::string GetString();
 
   // バッファからソケットにノンブロッキング送信する 引数はソケットのFD
   // 返り値がfalseの場合は、全ての文字列を送信できていないので、EPOLLOUTに登録し、再度送信する
@@ -65,13 +63,12 @@ public:
   int SendSocket(const int fd);
 
   // バッファの文字列をクリアする
-  void ClearBuff();
+    void ClearBuff();
+    std::size_t GetBuffSize();
 
-  std::size_t GetBuffSize();
-
-private:                                     // 使用予定無し
-  SocketBuff(const SocketBuff &);            // コピーコンストラクタ
-  SocketBuff &operator=(const SocketBuff &); // 代入演算子
+private:
+    SocketBuff(const SocketBuff &);
+    SocketBuff &operator=(const SocketBuff &);
 };
 
 #endif // SOCKETBUFF_HPP_
