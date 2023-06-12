f0c02aa1ad38405160284fc2cfed4a59d9f50f38
chanma <yunosuke.homma@icloud.com>
2023-06-12 13:02:35 +0900

    バグってたので直す

--- srcs/http/Request.cpp
+++ srcs/http/Request.cpp
@@ -472,11 +472,8 @@ std::string Request::ResolveHost() {
   if (!uri.host.empty()) {
     host = uri.host;
   }
-  std::string host_string = message_.header["Host"][0];
-  if (host_string.find(':') != std::string::npos) {
-    host = host_string.substr(0, host_string.find(':'));
-  } else {
-    host = host_string;
+  if (message_.header.find("Host") != message_.header.end()) {
+    host = message_.header["Host"][0];
   }
   return host;
 }
