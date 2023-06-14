41730d1f06e7929c2d3b3b0c964c74993c1446c7
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-06-14 10:25:21 +0900

    print debug 用のマクロを追加 ついでに不要なincludeも削除

--- srcs/utils/define.hpp
+++ srcs/utils/define.hpp
@@ -1,9 +1,17 @@
 #ifndef _DEFINE_HPP_
 #define _DEFINE_HPP_
 
-#include <iostream>
+
 
 #define FAILURE -1
 #define SUCCESS 0
+
+#ifndef DEBUG
+  #define DEBUG_PRINT(fmt, ...) \
+    do {} while(0);
+#else
+  #define DEBUG_PRINT(fmt, ...) \
+    do { printf(fmt, ##__VA_ARGS__); } while(0);
+#endif
 
 #endif
