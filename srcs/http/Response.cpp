9f6b0b40ec9da6715669b455379f2c832d73e6c4
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-06-14 10:18:56 +0900

    ブランチ名の通り

--- srcs/http/Response.cpp
+++ srcs/http/Response.cpp
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <vector>
+#include <cstdio>
 
 Response::Response() {
   process_status_ = PROCESSING;
@@ -221,7 +222,7 @@ void Response::DeleteFile(Request &request, const std::string &path) {
   if (IsDeleteableFile(request, path) == false) {
     return;
   }
-  if (unlink(path.c_str()) == -1) {
+  if (std::remove(path.c_str()) == -1) {
     SetResponseStatus(Http::HttpStatus(500));
   } else {
     SetResponseStatus(Http::HttpStatus(200));
